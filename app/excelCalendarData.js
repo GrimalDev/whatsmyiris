815c9c2e186608d535e5db86a7352a1987984615
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-01-23 18:27:01 +0100

    Download the Excel from sharepoint
    - pullExcelData is async function that uses a library called "sp-download" in hands with an sharepoint authentication library called "sp-auth".
    - config file is for the connection as microsoft user

---
+++ app/excelCalendarData.js
@@ -0,0 +1,29 @@
+import {AuthConfig as AuthConfig} from "node-sp-auth-config";
+import {Download as spDownload} from "sp-download";
+
+async function pullExcelData() {
+
+    const fileUrl = 'https://iecm064-my.sharepoint.com/personal/boris_mallick_mediaschool_education/Documents/Partages/Planning%20annuel%20BTS%20SIO%202022-2023.xlsx';
+    const filePath = './src/calendar/calendar-main.xlsx'
+
+    const spAuth = await new AuthConfig({
+        configPath: './config/sp-config.json',
+        encryptPassword: true,
+        saveConfigOnDisk: true
+    });
+
+    spAuth.getContext()
+        .then((context) => {
+            const calendarDL = new spDownload(context.authOptions);
+
+            calendarDL.downloadFile(fileUrl, filePath)
+                .then((savedToPath) => {
+                    console.log(`Downloaded to ${savedToPath}`);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
+        .catch(console.warn);
+}
+await pullExcelData();

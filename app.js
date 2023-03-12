b9411f0cf24b721aa557db3639f1fb863c4d889c
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-03-12 20:37:46 +0100

    remove config because auth are now in env

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
-// import sassMiddleware from 'node-sass-middleware';
 import createError from 'http-errors';
 import {fileURLToPath} from "url";
+import fs from "fs";
 
 //route imports
 import homeRouter from './routes/home.js';
 import calendarRouter from './routes/calendar.js';
+import pullExcelData from "./app/excelCalendarData.js";
+import extractDayInfos from "./app/calendarHandle.js";
 
 const app = express();
 const listeningPort = 80
@@ -21,15 +23,23 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//if state if in developpemnt use sass middleware
-// if (app.get('env') === 'development') {
-//   app.use(sassMiddleware({
-//     src: path.join(__dirname, 'public'),
-//     dest: path.join(__dirname, 'public'),
-//     indentedSyntax: false, // true = .sass and false = .scss
-//     sourceMap: true
-//   }));
-// }
+
+// if state is not in production use sass middleware
+if (process.env.NODE_ENV !== 'production') {
+  try {
+    await import('node-sass-middleware').then((sassMiddleware) => {
+      app.use(sassMiddleware({
+        src: __dirname + '/public',
+        dest: __dirname + '/public',
+        indentedSyntax: true,
+        sourceMap: true,
+      }));
+    });
+  } catch {
+    console.log('Ignoring Node Sass Middleware')
+  }
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRouter);
@@ -59,3 +69,15 @@ app.listen(listeningPort, (err) => {
   if (err) { throw err }
   console.log(`App running on http://localhost:${listeningPort} !`);
 })
+
+//download the excel file every hour
+setInterval(async () => {
+  await pullExcelData('src/calendar/main-calendar.xlsx');
+  const calendarJSON = await extractDayInfos('src/calendar/main-calendar.xlsx');
+  //write the calendarJSON to a file
+  fs.writeFile('src/calendar/calendar.json', JSON.stringify(calendarJSON), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}, 3600000);

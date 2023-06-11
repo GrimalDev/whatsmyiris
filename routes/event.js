35bbadb3bc883f62255f48aa98e4a6a35c2f5476
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-06-11 15:26:28 +0200

    Future route to add custom events

---
+++ routes/event.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+
+//dotenv config
+import dotenv from "dotenv";
+dotenv.config();
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+    //return the page to add an event
+
+});
+
+export default router;
